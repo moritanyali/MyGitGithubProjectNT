ac0b251f40578fa1809e51a0f02a90c6339d8dc8
Oguzhan Aydin <moritanyali26@yahoo.com>
2023-04-26 22:02:59 +0300

    v4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -7,6 +7,9 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java Earn Money");
         //version3
+        System.out.println("Git öğrenmek çok kolay;)");
+        //version4
+
     }
 
 
